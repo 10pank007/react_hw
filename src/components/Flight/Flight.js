8d438daad2972d9c44946b067539b3760411d143
Bohdan Pankiv <pankivbogdan96@gmail.com>
2023-10-19 21:41:01 +0300

    made 2hw spaceX_APA

--- src/components/Flight/Flight.js
+++ src/components/Flight/Flight.js
@@ -3,8 +3,7 @@ import React from 'react';
 import css from "./Flight.module.css"
 
 const Flight = ({iter}) => {
-    let {launch_year, mission_name, links} = iter;
-    let {mission_patch_small: img} = links;
+    let {launch_year, mission_name, links:{mission_patch_small:img}} = iter;
     return (
         <div className={css.Flight}>
             <h2>{launch_year} {mission_name} </h2>
